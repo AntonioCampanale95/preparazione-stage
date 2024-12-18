3f8bfd2b99ccce811efbb1a5e0906af2a40a5417
AntonioCampanale95 <163310701+AntonioCampanale95@users.noreply.github.com>
2024-12-18 12:27:58 +0100

    secondo commit
    Aggiunta protezione credenziali database (creato un .env per contenere utente, pass, url)
    Creato un gitignore

--- gestore-viaggi/src/main/java/com/example/GestoreDatabase.java
+++ gestore-viaggi/src/main/java/com/example/GestoreDatabase.java
@@ -1,17 +1,22 @@
 package com.example;
 
 import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 
 public class GestoreDatabase {
 
-    private static final String url = "jdbc:mysql://localhost:3306/gestore_viaggi";
-    private static final String user = "root";
-    private static final String pass = "antoniodev";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, user, pass);
+    static Dotenv dotenv = Dotenv.load();
+    
+        private static final String url = dotenv.get("DB_URL");
+        private static final String user = dotenv.get("DB_USERNAME");
+        private static final String pass = dotenv.get("DB_PASSWORD");
+    
+        public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(url, user, pass);
     }
 
 }
--- gestore-viaggi/src/main/java/com/example/Main.java
+++ gestore-viaggi/src/main/java/com/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         
         try {
-            ViaggioCSVReader.leggiCSVInserisciDB("gestore-viaggi\\viaggi.csv");
+            ViaggioCSVReader.leggiCSVInserisciDB("viaggi2.csv");
         } catch (CsvValidationException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
